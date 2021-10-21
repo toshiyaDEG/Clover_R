986ce01b0e453fb20552b1da1b59080240618b50
toshiya <arche.dsp@gmail.com>
2021-10-21 10:16:39 -0500

    Añadiendo html para subir avisos

--- index/urls.py
+++ index/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('avisos/', views.avisos, name="avisos"),
+    path('subiraviso/', views.subiraviso, name="nuevoaviso"),
     path("login/", auth_views.LoginView.as_view(template_name="login/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout")
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from . import models
+from usuarios.models import Account
+
 
 
 def index(request):
@@ -9,14 +12,18 @@ def index(request):
 
 @login_required
 def avisos(request):
-    """Atiende la petición GET /"""
+    """Atiende la petición GET /avisos"""
     #es_maestro = request.user.groups.filter(name="maestro").exists()
-    tipo = request.user.typo
-    if tipo == "teacher":
-        es_maestro = True
-    else:
-        es_maestro = False
+    es_maestro = Account.objects.filter(typo = "teacher").exists()
 
     return render(request, "avisos/avisos.html", { "es_maestro": es_maestro })
 
 
+
+@login_required
+def subiraviso(request):
+    """Atiende la petición GET /subiraviso"""
+    es_maestro = Account.objects.filter(typo = "teacher").exists()
+
+
+    return render(request, "avisos/subiraviso.html", { "es_maestro": es_maestro })
