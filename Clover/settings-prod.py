c846c4ad754ec4bebadae2601a33aadccf69e568
toshiya <arche.dsp@gmail.com>
2021-10-22 22:10:48 -0500

    Agregando en settings allowed hosts de pythonanywhere

--- Clover/settings-prod.py
+++ Clover/settings-prod.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://toshiyadeg.pythonanywhere.com"]
 
 
 # Application definition
