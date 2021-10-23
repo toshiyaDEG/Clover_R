619c47040c56bb7c20fdcdb525185bbb7c9c39c6
toshiya <arche.dsp@gmail.com>
2021-10-23 00:16:03 -0500

    Corrigiendo urls de usuarios para eliminar correctamente un alumno

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.registration_view, name="registro"),
     path('alumnos/', views.alumnos, name="alumnos"),
-    path('eliminar_alumno/<int:id_user>/', views.eliminar_alumno, name="eliminar_alumno"),
+    path('eliminar_alumno/<int:id_alumno>/', views.eliminar_alumno, name="eliminar_alumno"),
 ]
