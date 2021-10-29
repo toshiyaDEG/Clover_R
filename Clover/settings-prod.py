7d09e485b3b968c64a29daf281133cecf2a36dab
toshiya <arche.dsp@gmail.com>
2021-10-29 13:48:56 -0500

    Actualizando requirements y ajustando settings

--- Clover/settings-prod.py
+++ Clover/settings-prod.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) == 'False')
 
 ALLOWED_HOSTS = ["clover.pythonanywhere.com"]
 
